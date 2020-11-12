5b34248248742259712bc0c82aeb116a20042e99
roland-sipos <sipos.roland.cb@gmail.com>
2020-11-12 18:59:42 +0100

    update to daq_application

--- scripts/balancer.py
+++ scripts/balancer.py
@@ -7,37 +7,35 @@ import psutil
 import json
 
 parser = argparse.ArgumentParser(description='Alters CPU affinities of running processes based on a configuration file.')
-parser.add_argument('--host', type=str, default='localhost', help='target host/endpoint')
-parser.add_argument('-p', '--port', type=int, default=12345, help='target port')
-parser.add_argument('-a', '--answer-port', type=int, default=12333, help='answer to service listening on this port')
-parser.add_argument('-r', '--route', type=str, default='command', help='target route on endpoint')
-parser.add_argument('-f', '--file', type=str, help='file that contains command to be posted')
-parser.add_argument('-w', '--wait', type=int, default=2, help='seconds to wait between sending commands')
-parser.add_argument('-i', '--interactive', dest='interactive', action='store_true', help='interactive mode')
-parser.add_argument('--non-interactive', dest='interactive', action='store_false')
-parser.set_defaults(interactive=False)
+parser.add_argument('--process', '-p', type=str, required=True, help='target processes\' name')
+parser.add_argument('--pinfile', '-f', type=str, required=True, help='file with thread name to CPU mask list')
 
+try:
+  args = parser.parse_args()
+except:
+  parser.print_help()
+  sys.exit(0)
+
+print('Basic information...')
 lcpu_count = psutil.cpu_count(0)
 pcpu_count = psutil.cpu_count(logical=False)
 vmem = psutil.virtual_memory()
-print('Logical CPU count: ', lcpu_count)
-print('Physical CPU count: ', lcpu_count)
-print('Memory status: ', vmem) 
+print('  -> Logical CPU count: ', lcpu_count)
+print('  -> Physical CPU count: ', lcpu_count)
+print('  -> Memory status: ', vmem) 
 
-if len(sys.argv) != 2:  
-  quit()
+process_name = args.process
+pinfile = args.pinfile 
 
 try:
-  with open(sys.argv[1], 'r') as f:
+  with open(pinfile, 'r') as f:
     affinity_dict = json.load(f)
 except Exception as e:
   print(e)
-  quit()
+  sys.exit(0)
 
-print('#######################')
-process_name = "daq_application"
+print('\nAttempt to find processes and apply cpu mask...')
 pid = None
-
 for proc in psutil.process_iter():
   if process_name in proc.name():
 
