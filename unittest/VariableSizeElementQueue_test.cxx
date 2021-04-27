d9e0306910d036547e6414805f1180bd0fd1c429
Florian Grötschla <florian.till.grotschla@cern.ch>
2021-04-27 10:21:11 +0200

    Rename queue and make it more generic

--- unittest/VariableSizeElementQueue_test.cxx
+++ unittest/VariableSizeElementQueue_test.cxx
@@ -15,7 +15,7 @@
 #include "boost/test/unit_test.hpp"
 
 #include "readout/ReadoutTypes.hpp"
-#include "VariableSizeElementQueue.hpp"
+#include "SearchableProducerConsumerQueue.hpp"
 
 #include <string>
 #include <cstdio>
@@ -24,9 +24,17 @@ using namespace dunedaq::readout;
 
 BOOST_AUTO_TEST_SUITE(VariableSizeElementQueue_test)
 
+struct KeyGetter {
+  uint32_t operator() (types::VariableSizePayloadWrapper& wrapper) {
+    uint32_t timestamp;
+    memcpy(&timestamp, wrapper.data.get(), sizeof(uint32_t));
+    return timestamp;
+  }
+};
+
 BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_find) {
   TLOG() << "Fill up a queue and check if the elements can be found" << std::endl;
-  VariableSizeElementQueue<types::VariableSizePayloadWrapper> queue(10000);
+  SearchableProducerConsumerQueue<types::VariableSizePayloadWrapper, uint32_t, KeyGetter> queue(10000);
   for (uint32_t timestamp_counter = 0; timestamp_counter < 10000; timestamp_counter += 10) {
     char* payload = (char*) malloc(1024);
     memcpy(payload, &timestamp_counter, sizeof(uint32_t));
@@ -46,7 +54,7 @@ BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_find) {
 
 BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_find_upper) {
   TLOG() << "Search for elements that are not in the queue" << std::endl;
-  VariableSizeElementQueue<types::VariableSizePayloadWrapper> queue(10000);
+  SearchableProducerConsumerQueue<types::VariableSizePayloadWrapper, uint32_t, KeyGetter> queue(10000);
   for (uint32_t timestamp_counter = 10; timestamp_counter < 10000; timestamp_counter += 10) {
     char* payload = (char*) malloc(1024);
     memcpy(payload, &timestamp_counter, sizeof(uint32_t));
@@ -66,7 +74,7 @@ BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_find_upper) {
 
 BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_find_outside) {
   TLOG() << "Try to find elements that are bigger than the biggest one in the queue" << std::endl;
-  VariableSizeElementQueue<types::VariableSizePayloadWrapper> queue(10);
+  SearchableProducerConsumerQueue<types::VariableSizePayloadWrapper, uint32_t, KeyGetter> queue(10);
   char* payload = (char*) malloc(1024);
   uint32_t timestamp = 42;
   memcpy(payload, &timestamp, sizeof(uint32_t));
@@ -80,7 +88,7 @@ BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_find_outside) {
 
 BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_overrun) {
   TLOG() << "Cause an overrun of the cyclic buffer" << std::endl;
-  VariableSizeElementQueue<types::VariableSizePayloadWrapper> queue(1001);
+  SearchableProducerConsumerQueue<types::VariableSizePayloadWrapper, uint32_t, KeyGetter> queue(1001);
   for (uint32_t timestamp_counter = 0; timestamp_counter < 1000; ++timestamp_counter) {
     char* payload = (char*) malloc(1024);
     memcpy(payload, &timestamp_counter, sizeof(uint32_t));
@@ -113,37 +121,37 @@ BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_overrun) {
 }
 
 BOOST_AUTO_TEST_CASE(VariableSizeElementQueue_overrun_find_upper) {
-    TLOG() << "Cause an overrun of the cyclic buffer and search for upper timestamps" << std::endl;
-    VariableSizeElementQueue<types::VariableSizePayloadWrapper> queue(1001);
-    for (uint32_t timestamp_counter = 10; timestamp_counter < 10000; timestamp_counter += 10) {
-      char* payload = (char*) malloc(1024);
-      memcpy(payload, &timestamp_counter, sizeof(uint32_t));
-      types::VariableSizePayloadWrapper wrapper(1024, payload);
-      bool queue_write = queue.write(std::move(wrapper));
-      BOOST_REQUIRE(queue_write);
-    }
+  TLOG() << "Cause an overrun of the cyclic buffer and search for upper timestamps" << std::endl;
+  SearchableProducerConsumerQueue<types::VariableSizePayloadWrapper, uint32_t, KeyGetter> queue(1001);
+  for (uint32_t timestamp_counter = 10; timestamp_counter < 10000; timestamp_counter += 10) {
+    char* payload = (char*) malloc(1024);
+    memcpy(payload, &timestamp_counter, sizeof(uint32_t));
+    types::VariableSizePayloadWrapper wrapper(1024, payload);
+    bool queue_write = queue.write(std::move(wrapper));
+    BOOST_REQUIRE(queue_write);
+  }
 
-    for (int i = 0; i < 500; ++i) {
-      types::VariableSizePayloadWrapper wrapper;
-      bool read_successful = queue.read(wrapper);
-      BOOST_REQUIRE(read_successful);
-    }
+  for (int i = 0; i < 500; ++i) {
+    types::VariableSizePayloadWrapper wrapper;
+    bool read_successful = queue.read(wrapper);
+    BOOST_REQUIRE(read_successful);
+  }
 
-    for (uint32_t timestamp_counter = 10000; timestamp_counter < 15000; timestamp_counter += 10) {
-      char* payload = (char*) malloc(1024);
-      memcpy(payload, &timestamp_counter, sizeof(uint32_t));
-      types::VariableSizePayloadWrapper wrapper(1024, payload);
-      bool queue_write = queue.write(std::move(wrapper));
-      BOOST_REQUIRE(queue_write);
-    }
+  for (uint32_t timestamp_counter = 10000; timestamp_counter < 15000; timestamp_counter += 10) {
+    char* payload = (char*) malloc(1024);
+    memcpy(payload, &timestamp_counter, sizeof(uint32_t));
+    types::VariableSizePayloadWrapper wrapper(1024, payload);
+    bool queue_write = queue.write(std::move(wrapper));
+    BOOST_REQUIRE(queue_write);
+  }
 
-    for (uint32_t expected_timestamp = 5005; expected_timestamp < 14995; expected_timestamp += 10) {
-      uint32_t timestamp;
-      types::VariableSizePayloadWrapper* found_element = queue.find(expected_timestamp);
-      BOOST_REQUIRE(found_element);
-      std::memcpy(&timestamp, found_element->data.get(), sizeof(timestamp));
-      BOOST_REQUIRE_EQUAL(expected_timestamp+5, timestamp);
-    }
+  for (uint32_t expected_timestamp = 5005; expected_timestamp < 14995; expected_timestamp += 10) {
+    uint32_t timestamp;
+    types::VariableSizePayloadWrapper* found_element = queue.find(expected_timestamp);
+    BOOST_REQUIRE(found_element);
+    std::memcpy(&timestamp, found_element->data.get(), sizeof(timestamp));
+    BOOST_REQUIRE_EQUAL(expected_timestamp+5, timestamp);
+  }
 }
 
 
