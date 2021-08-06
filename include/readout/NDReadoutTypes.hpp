a501bc9db2b133121f31febbc3b6697d727916a9
Florian Grötschla <florian.till.grotschla@cern.ch>
2021-08-06 14:30:06 +0200

    #NOLINT

--- include/readout/NDReadoutTypes.hpp
+++ include/readout/NDReadoutTypes.hpp
@@ -37,18 +37,18 @@ struct PACMAN_MESSAGE_STRUCT
   {
     auto thisptr = reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&data);        // NOLINT
     auto otherptr = reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&other.data); // NOLINT
-    return (thisptr->get_msg_header((void*)&data)->unix_ts) < (otherptr->get_msg_header((void*)&other.data)->unix_ts)
+    return (thisptr->get_msg_header((void*)&data)->unix_ts) < (otherptr->get_msg_header((void*)&other.data)->unix_ts) // NOLINT
              ? true
-             : false; // NOLINT
+             : false;
   }
 
   // message UNIX timestamp - NOT individual packet timestamps
   uint64_t get_timestamp() const // NOLINT(build/unsigned)
   {
     return (
-      (uint64_t)(
-        reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&data)->get_msg_header((void*)&data)->unix_ts) *
-      1000000000); // NOLINT
+      (uint64_t)( // NOLINT
+        reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&data)->get_msg_header((void*)&data)->unix_ts) * // NOLINT
+      1000000000);
   }
 
   // FIX ME - implement this in the frame later
@@ -59,17 +59,17 @@ struct PACMAN_MESSAGE_STRUCT
 
   uint64_t get_message_type() const // NOLINT(build/unsigned)
   {
-    return reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&data)
-      ->get_msg_header((void*)&data)
-      ->type; // NOLINT
+    return reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&data) // NOLINT
+      ->get_msg_header((void*)&data) // NOLINT
+      ->type;
   }
   void inspect_message() const
   {
     TLOG_DEBUG(1) << "Message timestamp: " << get_timestamp();
 
-    TLOG_DEBUG(1) << "Message Type: " << (char)get_message_type();
+    TLOG_DEBUG(1) << "Message Type: " << (char)get_message_type(); // NOLINT
 
-    uint16_t numWords =
+    uint16_t numWords = // NOLINT
       reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&data)->get_msg_header((void*)&data)->words; // NOLINT
 
     TLOG_DEBUG(1) << "Num words in message: " << numWords;
@@ -80,8 +80,8 @@ struct PACMAN_MESSAGE_STRUCT
       dunedaq::dataformats::PACMANFrame::PACMANMessageWord* theWord =
         reinterpret_cast<const dunedaq::dataformats::PACMANFrame*>(&data)->get_msg_word((void*)&data, i); // NOLINT
 
-      TLOG_DEBUG(1) << "Word type: " << (char)theWord->data_word.type;
-      TLOG_DEBUG(1) << "PACMAN I/O Channel: " << (char)theWord->data_word.channel_id;
+      TLOG_DEBUG(1) << "Word type: " << (char)theWord->data_word.type; // NOLINT
+      TLOG_DEBUG(1) << "PACMAN I/O Channel: " << (char)theWord->data_word.channel_id; // NOLINT
       TLOG_DEBUG(1) << "Word receipt timestamp: " << theWord->data_word.receipt_timestamp;
 
       dunedaq::dataformats::PACMANFrame::LArPixPacket* thePacket = &(theWord->data_word.larpix_word);
