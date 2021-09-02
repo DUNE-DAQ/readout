4b7a2d67a4f0c05a7aad397b6f81c52e43c43cbc
Florian Grötschla <florian.till.grotschla@cern.ch>
2021-09-02 09:40:56 +0200

    Use right constructor

--- src/wib/WIBFrameProcessor.hpp
+++ src/wib/WIBFrameProcessor.hpp
@@ -274,7 +274,7 @@ public:
       m_channel_map.reset(new swtpg::PdspChannelMapService(channel_map_rce, channel_map_felix));
 
       m_induction_items_to_process =
-        std::make_unique<IterableQueueModel<InductionItemToProcess>>(200000, 64); // 64 byte aligned
+        std::make_unique<IterableQueueModel<InductionItemToProcess>>(200000, false, 0, true, 64); // 64 byte aligned
 
       // Setup parallel post-processing
       TaskRawDataProcessorModel<types::WIB_SUPERCHUNK_STRUCT>::add_postprocess_task(
